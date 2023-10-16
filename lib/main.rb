1df006a5d8dcd15e776a10982c9e9091b653e8a8
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2023-10-16 03:17:02 -0700

    Add game, board, player classes and their initialized variables

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,30 @@
 # classes
 
 class Game
-  # runs game
+  attr_accessor :player1, :player2, :board
+  
+  def initialize
+    @round = 0
+  end 
+
+  # test this to see if it's changing instance variables
+  def get_names
+    puts 'Welcome to Connect Four!'
+    puts 'Player 1 what is your name?'
+    players = Players.new
+    @player1 = gets.chomp
+    puts 'Player 2 what is your name?'
+    @player2 = gets.chomp
+  end
+
+  # Looping script method
+  # check whether object is recieving calls to method with double instance
+  def play_game
+    get_names
+    board = Board.new
+    board.display_board
+
+  end
 
   # announcements
 
@@ -11,11 +34,27 @@ class Game
 end
 
 class Board
-  # Create board 6 x 7 with empty circles
+  attr_accessor :player1, :player2
 
-  # display board
-    # in reverse, without commas, array lines
-    # 0-6 under 
+
+  def initialize
+    @board = [
+      ['◯', '◯', '◯', '◯', '◯', '◯', '◯'],
+      ['◯', '◯', '◯', '◯', '◯', '◯', '◯'],
+      ['◯', '◯', '◯', '◯', '◯', '◯', '◯'],
+      ['◯', '◯', '◯', '◯', '◯', '◯', '◯'],
+      ['◯', '◯', '◯', '◯', '◯', '◯', '◯'],
+      ['◯', '◯', '◯', '◯', '◯', '◯', '◯']
+    ]
+  end
+  
+  def display_board
+    reverse_board = @board.reverse
+    reverse_board.each do |row|
+      puts row.join(' ')
+    end
+    puts "0 1 2 3 4 5 6"
+  end
 
   # update input
     # array position Board[6][input] or x- 1 until not empty
@@ -40,9 +79,19 @@ class Board
 
 end
 
-class Player
+class Players
+
+  def initialize
+    @player1 = nil
+    @player2 = nil
+  end 
+
+
   # Player Name
 
   # take input 0-6
 
 end
+
+game = Game.new
+game.play_game
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,1 +1,9 @@
 require './lib/main.rb'
+
+
+# test get_names to see if it's changing instance variables
+
+
+# Board 
+# input
+# win conditions
